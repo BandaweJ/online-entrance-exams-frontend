83d5c58821448dab23b71b7428332dd64713d31e
BandaweJ <bands500@gmail.com>
2025-10-02 13:58:22 +0200

    Add missing environment imports to all services

--- src/app/core/services/student.service.ts
+++ src/app/core/services/student.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Student, CreateStudentRequest } from '../../models/student.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
